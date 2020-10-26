7a8da984b5090444a3d6e7fc0ac31ee0fcd361b0
MadisonD2024 <mld9095@nyu.edu>
2020-10-25 20:11:20 -0400

    Made an Array for Balls

--- sketch.js
+++ sketch.js
@@ -4,22 +4,25 @@
 // let x1, y1;
 // let dy1 = 0
 
+let balls = [];
 let ball0;
 let ball1;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
 
+  balls = [
   ball0 = {
     x: random(0, 10),
     y: random(height),
     dy: 0
-  }
+  },
   ball1 = {
     x: random(0, 10),
     y: random(height),
     dy: 0
   }
+]
 
     // x0 = random(width);
   // x1 = random(width);
@@ -31,19 +34,52 @@ function setup() {
 
 function draw() {
   background(200);
+for (let i = 0; i < balls.length; i++) {
+  circle(balls[i].x,balls[i].y,30) }
+
+  // balls.x += balls.dy;
+  // balls.dy += 0.3;
+  // circle(balls.x,balls.y,30);
+
   ball0.x += ball0.dy;
-  ball0.dy += 0.3;
+  ball0.dy += 5;
   circle(ball0.x,ball0.y,30);
 
   ball1.x += ball1.dy;
-  ball1.dy += 0.3;
+  ball1.dy += 5;
   circle(ball1.x,ball1.y,30);
 
+  // if (balls.x >= width) {
+  //   balls.dy = -0.95 * balls.dy; }
+
   if (ball0.x >= width) {
-    ball0.dy = -0.95 * ball0.dy;
+    ball0.dy = -0.95 * ball0.dy
   }
 
   if (ball1.x >= width) {
-    ball1.dy = -0.95 * ball1.dy;
+    ball1.dy = -0.95 * ball1.dy 
   }
-}
+
+    // if (ball0.x < 0) {
+    //   ball0.dy = 0.95 * ball0.dy
+    // }
+  
+    // if (ball1.x < 0) {
+    //   ball0.dy = 0.95 * ball0.dy
+    // }
+
+    // if (ball0.x >= width) {
+    //   ball0.dy = 0.95 * ball0.dy
+    // }
+  
+    // if (ball1.x >= width) {
+    //   ball1.dy = 0.95 * ball1.dy; }
+
+    // if (ball0.x >= width) {
+    //   ball0.dy = 0.95 * ball0.dy;
+    // }
+  
+    // if (ball1.x >= width) {
+    //   ball1.dy = 0.95 * ball1.dy;
+  } 
+// }
