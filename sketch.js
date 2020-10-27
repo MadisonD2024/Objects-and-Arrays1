25d8f066309cad29f0f03aad17accb8fe219402c
MadisonD2024 <mld9095@nyu.edu>
2020-10-26 21:14:12 -0400

    created array, class, displayed objects, bgcolor

--- sketch.js
+++ sketch.js
@@ -1,86 +1,59 @@
 
-let balls = [];
-let ball0;
-let ball1;
-let myBall;
+var balls = [];
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  for (let i = 0; i < 20; i++) {
-    balls[i] = new Ball(width/2, height/2); }
 
-myBall = new Ball(100,250, 50);
-
-  balls = [
-  ball0 = {
-    x: random(0, 10),
-    y: random(height),
-    dy: 0
-  },
-  ball1 = {
-    x: random(0, 10),
-    y: random(height),
-    dy: 0
+  for (var i = 0; i < 20; i++) {
+    balls[i] = {
+      x: random(0, width),
+      y: random(0, height),
+      dy: 0,
+      display: function () {
+        circle(this.x, this.y, 30);
+        stroke(225);
+      }
+    }
   }
-]
 }
 
 function draw() {
-  background(200);
-  for (let ball of balls) {
-  myBall.update();
-  myBall.display(); }
+  colorMode(RGB)
+  background(200, 100, 150);
 
-for (let i = 0; i < balls.length; i++) {
-  circle(balls[i].x,balls[i].y,30) }
+  for (var i = 0; i < 10; i++) {
+    balls[i].x += balls[i].dy;
+    balls[i].dy += 5;
+    balls[i].display();
+    d = random(0, 60)
+    circle(balls[i].x, balls[i].y, d);
 
-  ball0.x += ball0.dy;
-  ball0.dy += 5;
-  circle(ball0.x,ball0.y,30);
-
-  ball1.x += ball1.dy;
-  ball1.dy += 5;
-  circle(ball1.x,ball1.y,30);
-
-
-  if (ball0.x >= width) {
-    ball0.dy = -0.96 * ball0.dy
-  }
-
-  if (ball1.x >= width) {
-    ball1.dy = -0.96 * ball1.dy
-  }
-
-    if (ball0.x <= 0) {
-      ball0.dy = 0.96 * ball0.dy
-    }
-  
-    if (ball1.x <= 0) {
-      ball1.dy = 0.96 * ball1.dy
+    if (balls[i].x >= width) {
+      balls[i].dy = -0.96 * balls[i].dy
     }
 
-  } 
-  function mousePressed() {
-    if (mouseIsPressed) {
-      colorMode(RGB);
-      a = random(225);
-      b = random(225);
-      c = random(225);
-      fill(a,b,c);
+    if (balls[i].x <= 0) {
+      balls[i].dy = 0.96 * balls[i].dy
     }
   }
-  class Ball {
-    construtor(x,y,diameter) {
-      this.x = 100;
-      this.y = 250;
-      this.diameter = 50;
-    }
+}
+function mousePressed() {
+  if (mouseIsPressed) {
+    colorMode(RGB);
+    a = random(225);
+    b = random(225);
+    c = random(225);
+    fill(a, b, c);
+  }
+}
 
-      update() {
-      this.x = this.x + 5
-      this.y = this.y + 10
-      }
-      display() {
-        circle(this.x,this.y,this.diameter)
-      }
+class Ball {
+  constructor(x, y) {
+    this.x = random(0, width);
+    this.y = random(0, height);
+    this.display = function () {
+      stroke(225);
+      circle(this.x, this.y, 20);
     }
+  }
+}
